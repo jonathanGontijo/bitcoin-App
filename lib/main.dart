20420409d8aae5d95e8827bf861dfa5a56ae0525
jonathanGontijo <jonathan.ferreira1312@hotmail.com>
2021-04-17 13:44:34 -0300

    requisicao api bitcoin

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:requisicao_api/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Home(),
+    debugShowCheckedModeBanner: false,
+    )
+  );
+}
+
